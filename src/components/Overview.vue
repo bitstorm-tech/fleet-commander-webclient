3b088ff2cd90db82555a4c0e8a45dda65f31505d
Josef Bauer <bauerjoe@gmx.de>
2018-12-01 23:10:12 +0100

    First tries with three.js

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,101 +1,106 @@
 <template>
-	<div id="root">
-		<fc-menu></fc-menu>
-		<div id="content">
-			<div class="fc-panel resources">
-				<table>
-					<tr>
-						<th>Type</th>
-						<th>Resources</th>
-						<th>Resources Per Minute</th>
-					</tr>
-					<tr>
-						<td>Titanium</td>
-						<td>{{$store.state.resources.titanium}}</td>
-						<td>{{titaniumPerMinute}}</td>
-					</tr>
-					<tr>
-						<td>Fuel</td>
-						<td>{{$store.state.resources.fuel}}</td>
-						<td>{{fuelPerMinute}}</td>
-					</tr>
-					<tr>
-						<td>Energy</td>
-						<td>{{$store.state.resources.energy}}</td>
-						<td>{{energyPerMinute}}</td>
-					</tr>
-				</table>
-			</div>
-			<div class="fc-panel resources">
-				<table>
-					<tr>
-						<th>Type</th>
-						<th>Ships</th>
-					</tr>
-					<tr>
-						<td>Titanium Harvester</td>
-						<td>{{$store.state.ships.titaniumHarvester}}</td>
-					</tr>
-					<tr>
-						<td>Fuel Harvester</td>
-						<td>{{$store.state.ships.fuelHarvester}}</td>
-					</tr>
-				</table>
-			</div>
-		</div>
-	</div>
+  <div id="root">
+    <fc-menu></fc-menu>
+    <div id="content">
+      <div class="fc-panel resources">
+        <table>
+          <tr>
+            <th>Type</th>
+            <th>Resources</th>
+            <th>Resources Per Minute</th>
+          </tr>
+          <tr>
+            <td>Titanium</td>
+            <td>{{$store.state.resources.titanium}}</td>
+            <td>{{titaniumPerMinute}}</td>
+          </tr>
+          <tr>
+            <td>Fuel</td>
+            <td>{{$store.state.resources.fuel}}</td>
+            <td>{{fuelPerMinute}}</td>
+          </tr>
+          <tr>
+            <td>Energy</td>
+            <td>{{$store.state.resources.energy}}</td>
+            <td>{{energyPerMinute}}</td>
+          </tr>
+        </table>
+      </div>
+      <div class="fc-panel resources">
+        <table>
+          <tr>
+            <th>Type</th>
+            <th>Ships</th>
+          </tr>
+          <tr>
+            <td>Titanium Harvester</td>
+            <td>{{$store.state.ships.titaniumHarvester}}</td>
+          </tr>
+          <tr>
+            <td>Fuel Harvester</td>
+            <td>{{$store.state.ships.fuelHarvester}}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-	import fcMenu from "@/components/Menu.vue"
-	import { Component, Vue } from "vue-property-decorator"
+import fcMenu from "@/components/Menu.vue";
+import { Component, Vue } from "vue-property-decorator";
 
-	@Component({
-		components: {
-			fcMenu
-		}
-	})
-	export default class Overview extends Vue {
-		get titaniumPerMinute(): number {
-			return this.$store.state.ships.titaniumHarvester *
-				this.$store.state.gameRules.titaniumHarvester.harvestPerMinute
-		}
+@Component({
+  components: {
+    fcMenu
+  }
+})
+export default class Overview extends Vue {
+  get titaniumPerMinute(): number {
+    return (
+      this.$store.state.ships.titaniumHarvester *
+      this.$store.state.gameRules.titaniumHarvester.harvestPerMinute
+    );
+  }
 
-		get fuelPerMinute(): number {
-			return this.$store.state.ships.fuelHarvester *
-				this.$store.state.gameRules.fuelHarvester.harvestPerMinute
-		}
+  get fuelPerMinute(): number {
+    return (
+      this.$store.state.ships.fuelHarvester *
+      this.$store.state.gameRules.fuelHarvester.harvestPerMinute
+    );
+  }
 
-		get energyPerMinute(): number {
-			return this.$store.state.motherShip.titaniumPerMinute
-		}
-	}
+  get energyPerMinute(): number {
+    return this.$store.state.motherShip.titaniumPerMinute;
+  }
+}
 </script>
 
 <style scoped>
-	#root {
-		display: flex;
-		padding: 15px;
-		width: 100%;
-		height: 100%;
-	}
+#root {
+  display: flex;
+  padding: 15px;
+  width: 100%;
+  height: 100%;
+}
 
-	#content {
-		flex-direction: column;
-		width: 100%;
-	}
+#content {
+  flex-direction: column;
+  width: 100%;
+}
 
-	.resources {
-		flex-direction: column;
-		padding: 15px;
-		margin-left: 10px;
-	}
+.resources {
+  flex-direction: column;
+  padding: 15px;
+  margin-left: 10px;
+}
 
-	td, th {
-		text-align: left;
-	}
+td,
+th {
+  text-align: left;
+}
 
-	button {
-		width: 200px;
-	}
+button {
+  width: 200px;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,51 @@
 import Vue from "vue"
-import App from "@/App.vue"
-import router from "@/router"
-import store from "@/store"
 import axios from "axios"
-import { ResourcesManager } from "@/shared/services/resources-manager"
+import * as THREE from "three"
+import { Camera, Mesh, Scene, WebGLRenderer } from "three"
+import Login from "@/components/Login.vue"
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = "http://localhost:8080"
 
-new Vue({
-	router,
-	store,
-	render: (h) => h(App)
-}).$mount("#app")
+// new Vue({
+// 	router,
+// 	store,
+// 	render: (h) => h(App)
+// }).$mount("#app")
 
-ResourcesManager.init(store, 2)
-ResourcesManager.startCounting()
+// ResourcesManager.init(store, 2)
+// ResourcesManager.startCounting()
+
+let camera: Camera, renderer: WebGLRenderer, mesh: Mesh, scene: Scene
+
+function init() {
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10)
+	camera.position.z = 1
+
+	scene = new THREE.Scene()
+	const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+	const material = new THREE.MeshNormalMaterial()
+	mesh = new THREE.Mesh(geometry, material)
+
+	scene.add(mesh)
+
+	renderer = new THREE.WebGLRenderer({antialias: true})
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	document.body.appendChild(renderer.domElement)
+
+	const login = new Login()
+	login.$mount(document.body)
+}
+
+function animate() {
+	requestAnimationFrame(animate)
+
+	mesh.rotation.x += 0.01
+	mesh.rotation.y += 0.02
+
+	renderer.render(scene, camera)
+}
+
+init()
+animate()
